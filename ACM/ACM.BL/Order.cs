ad5bf80002adf2adfec39370abcdd9a9c103c6bf
jatin96 <jatin-jt.narula@gmail.com>
2020-04-23 19:32:05 +0530

    adding other entity classes

---
+++ ACM/ACM.BL/Order.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    class Order
+    {
+        public Order()
+        {
+
+        }
+        public Order(int orderId)
+        {
+            orderId = orderId;
+        }
+        public DateTimeOffset? OrderDate { set; get; }
+        public int OrderId { get; private set; }
+        public Order Retrieve(int orderId)
+        {
+            return new Order();
+        }
+        public bool Save()
+        {
+            return true;
+        }
+        public bool Validate()
+        {
+            var isValid = true;
+            if (OrderDate == null) isValid = false;
+            return isValid;
+        }
+
+    }
+
+}
